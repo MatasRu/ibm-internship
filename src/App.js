494e102c31fa99dae3abb5f01f9562551fa42282
MatasRu <matas.rudaminas@gmail.com>
2021-07-11 17:46:15 +0300

    Updated back end to log user keywords in search and log pressed articles

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Search from "./components/Search";
 import Results from "./components/Results";
-import {useState} from "react"
+import {useEffect, useState} from "react"
+import http from "./plugins/Fetch";
 
 function App() {
 
     const [getData, setData] = useState([])
+    const [getIp, setIp] = useState("")
+
+    useEffect(() => {
+        http.get("https://ipgeolocation.abstractapi.com/v1/?api_key=6d036ed603ec4d0aa62da2fdeebe0c4c").then(res => {
+            setIp(res.ip_address)
+        })
+    }, [])
+
 
     return (
         <div className="App mt-5">
-            <Search setData={setData}/>
-            <Results getData={getData}/>
+            <Search getIp={getIp} setData={setData}/>
+            <Results getIp={getIp} getData={getData}/>
         </div>
     );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,16 +1,29 @@
 import "./style.sass"
+import http from "../plugins/Fetch";
 
-function Results({getData}) {
+function Results({getData, getIp}) {
 
-    const openInNewTab = (url) => {
-        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    const openInNewTab = (data) => {
+        logArticle(data)
+        const newWindow = window.open(data.url, '_blank', 'noopener,noreferrer')
         if (newWindow) newWindow.opener = null
+    }
+
+    function logArticle(data) {
+        http.post('http://localhost:3001/logArticle', {
+            pressedArticle: data.title,
+            url: data.url,
+            ipaddress: getIp
+        }).then(res => {
+            console.log(res)
+        })
     }
 
     return (
         <div className="d-flex mt-5 justify-content-center flex-wrap w-100 mb-5">
             {!!getData.articles ? getData.articles.map((item, index) =>
-                <div onClick={() => openInNewTab(item.url)} className="m-3 d-flex align-items-center flex-column box" key={index}>
+                <div onClick={() => openInNewTab(item)} className="m-3 d-flex align-items-center flex-column box"
+                     key={index}>
                     <span className="published">Published at : {item.publishedAt}</span>
                     <img className="image" src={item.image} alt=""/>
                     <div className="fw-bold h3 mt-2 ms-3">{item.title}</div>
